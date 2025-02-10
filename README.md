045e71b0524341e5f243a19a1c9c58ed74bc18e1
Maeevick <maeevick@gmail.com>
2025-02-10 22:07:59 +0100

    chore: sveltekit scaffolding with sv

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # the-tinkerers-tournament
+
 The Tinkerer's Tournament (TTT)
+
+## Boostrapped with `sv`
+
+- [x] prettier
+- [x] eslint
+- [x] vitest
+- [x] tailwind
---
+++ src/routes/+page.svelte
@@ -0,0 +1,4 @@
+<h1>Welcome to The Tinkekerer's Tournament (TTT)</h1>
+<p>
+	Backstage at <a href="https://maeevick.substack.com">Maeevick's Bazaar</a> to read the adventure
+</p>
